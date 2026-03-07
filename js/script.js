96aafe20f253b687a5082638274544323157e47e
ART <artkub00.non@gmail.com>
2026-03-07 13:33:38 +0700

    football website v2

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log("Website loaded successfully");
+console.log("Football website loaded");
+
+document.querySelectorAll(".card").forEach(card=>{
+card.addEventListener("click",()=>{
+alert("More football news coming soon!");
+});
+});
